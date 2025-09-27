7ba24fa0c31b42892f9d9562599ff165374277a6
pouceheure <hugo.pousseur@gmail.com>
2025-09-27 07:20:25 +0200

    fix highligh issue

--- static/js/highlight-navbar.js
+++ static/js/highlight-navbar.js
@@ -1,18 +1,29 @@
-function get_base_url(url) {
-  const splitted = url.split("/");
-  return splitted.length < 2 ? "" : splitted[1];
-}
+function highlightNavbar(basePath) {
+  function stripBase(path, basePath) {
+    if (basePath.endsWith("/")) {
+      basePath = basePath.slice(0, -1); 
+    }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const currentPath = window.location.pathname;
-  const currentPathBase = get_base_url(currentPath);
+    if (path.startsWith(basePath)) {
+      path = path.substring(basePath.length);
+    }
 
-  const navLinks = document.querySelectorAll('a.nav-link');
 
-  navLinks.forEach(link => {
-    const linkBaseUrl = get_base_url(link.getAttribute("href"));
-    if (linkBaseUrl === currentPathBase) {
+    if (path.length > 1 && path.endsWith("/")) {
+      path = path.slice(0, -1);
+    }
+
+    return path || "/";
+  }
+
+  const currentPath = stripBase(window.location.pathname, basePath);
+
+  document.querySelectorAll("a.nav-link").forEach(link => {
+    const linkUrl = new URL(link.getAttribute("href"), window.location.origin);
+    const linkPath = stripBase(linkUrl.pathname, basePath);
+
+    if (currentPath === linkPath) {
       link.classList.add("active");
     }
   });
-});
+}
