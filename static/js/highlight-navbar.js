6daa7fca6e08bba7d17e10cd2bd1a1e5fbd678c0
pouceheure <hugo.pousseur@gmail.com>
2025-09-27 08:06:04 +0200

    fix highligh issue

--- static/js/highlight-navbar.js
+++ static/js/highlight-navbar.js
@@ -1,13 +1,12 @@
 function highlightNavbar(basePath) {
   function stripBase(path, basePath) {
     if (basePath.endsWith("/")) {
-      basePath = basePath.slice(0, -1); 
+      basePath = basePath.slice(0, -1);
     }
 
     if (path.startsWith(basePath)) {
       path = path.substring(basePath.length);
     }
-
 
     if (path.length > 1 && path.endsWith("/")) {
       path = path.slice(0, -1);
@@ -16,14 +15,21 @@ function highlightNavbar(basePath) {
     return path || "/";
   }
 
-  const currentPath = stripBase(window.location.pathname, basePath);
+  function get_base_url(url){ splitted = url.split("/"); if(splitted.length < 2){ return ""; } return splitted[1]; }
+
+  currentPath = stripBase(window.location.pathname, basePath);
+  url = new URL(basePath);
+  currentPath = "/" + currentPath.slice(url.pathname.length)
+  currentPath = "/" + get_base_url(currentPath)
 
   document.querySelectorAll("a.nav-link").forEach(link => {
     const linkUrl = new URL(link.getAttribute("href"), window.location.origin);
-    const linkPath = stripBase(linkUrl.pathname, basePath);
+    linkPath = stripBase(linkUrl.pathname, basePath);
+    linkPath = "/" + linkPath.slice(url.pathname.length)
 
-    if (currentPath === linkPath) {
-      link.classList.add("active");
-    }
+      if (currentPath == linkPath) {
+        link.classList.add("active");
+      }
+    
   });
 }
